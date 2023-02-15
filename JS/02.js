af58db4468b51e3a2da6783403155d99287373e2
martinico83 <martin.nicolas.os83@gmail.com>
2023-02-15 16:30:20 -0300

    Variables con let y const

---
+++ JS/02.js
@@ -0,0 +1,17 @@
+// Variables con let
+
+// Variables
+
+let producto = 'Audifonos Gamer';
+
+let disponible;
+
+// producto = true;
+
+let producto1 = 'computadora', disponible1 = true, categoria = 'computadoras';
+
+// Estilos para las variables
+
+let nombre_producto = 'Monitor HD'; // underscore
+let nombreProducto = 'Mouse'; // Camelcase
+
---
+++ JS/03.js
@@ -0,0 +1,17 @@
+// Variables con let
+
+// Variables
+
+const producto = 'Audifonos Gamer';
+
+// const disponible; No debe usarse const con una variable sin inicializar y tampoco reasignar un valor 
+
+// producto = true;
+
+const producto1 = 'computadora', disponible1 = true, categoria = 'computadoras';
+
+// Estilos para las variables
+
+const nombre_producto = 'Monitor HD'; // underscore
+const nombreProducto = 'Mouse'; // Camelcase
+
