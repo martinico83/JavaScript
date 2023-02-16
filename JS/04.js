c3732431671f069e524e407f283514ec90115a30
Martin <martin.nicolas.os83@gmail.com>
2023-02-16 11:26:24 +0000

    String o Cadenas de Texto

---
+++ JS/04.js
@@ -0,0 +1,23 @@
+// String o cadenas de texto
+
+//const producto = "Monitor de 20 pulgadas";
+//const producto = "Monitor de 20\"";
+const producto = 'Monitor de 20"';
+
+
+/*
+const producto2 = String('Monitor de 30 pulgadas');
+const producto3 = new String('Monitor de 50 pulgadas');
+*/
+
+/*
+console.log(typeof producto);
+console.log(typeof producto2);
+console.log(typeof producto3);
+*/
+
+console.log(producto);
+/*
+console.log(producto2);
+console.log(producto3);
+*/
