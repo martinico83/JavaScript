357691615cbd877d58a315d89676b96270c34e84
Martin <martin.nicolas.os83@gmail.com>
2023-02-16 16:19:46 +0000

    Numeros y Operadores

---
+++ JS/05.js
@@ -0,0 +1,14 @@
+const nombre = 'Martin';
+const numero1 = '100';
+const numero2 = 200;
+const numero3 = 200.56;
+const numero4 = 7;
+const numero5 = 6;
+
+console.log(nombre);
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero2 + numero3);
+console.log(numero2 - numero3);
+console.log(numero4 - numero5);
