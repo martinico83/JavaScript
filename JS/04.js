822f1a292a2ae1413ae159eeffd6d01bf914874c
Martin <martin.nicolas.os83@gmail.com>
2023-02-16 11:49:43 +0000

    Metodos para los Strings

--- JS/04.js
+++ JS/04.js
@@ -2,7 +2,8 @@
 
 //const producto = "Monitor de 20 pulgadas";
 //const producto = "Monitor de 20\"";
-const producto = 'Monitor de 20"';
+const tweet = 'Aprendiendo JavaScript con el curso de Desarrollo Web completo';
+const producto2 = 'Monitor HD';
 
 
 /*
@@ -16,8 +17,28 @@ console.log(typeof producto2);
 console.log(typeof producto3);
 */
 
-console.log(producto);
+// length se usa para la extension
+console.log(tweet.length);
+console.log(producto2);
 /*
 console.log(producto2);
 console.log(producto3);
 */
+
+// IndexOf (retorna una posicion)
+console.log(tweet.indexOf('JavaScript'));
+console.log(producto2.indexOf('tablet'));
+
+// Includes (retorna true o false)
+console.log(tweet.includes('JavaScript'));
+console.log(producto2.includes('tablet'));
+
+
+
+
+
+
+
+
+
+
