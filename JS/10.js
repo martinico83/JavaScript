34e31f367dbd25b62c0fce12cf723b5ac00c246e
Martin <martin.nicolas.os83@gmail.com>
2023-02-22 11:40:14 +0000

    Modificar Objetos

--- JS/10.js
+++ JS/10.js
@@ -10,9 +10,15 @@ const producto = {
   disponible : true
 }
 
-console.log(producto);
+/* console.log(producto);
 console.log(producto.precio);
 console.log(producto.nombreProducto);
-console.log(producto.disponible);
+console.log(producto.disponible); */
 
-console.log(producto['precio']);
+//Agregar nuevas propiedades
+producto.imagen = 'imagen.jpg';
+
+//Eliminar propiedades
+delete producto.disponible;
+
+console.log(producto);
