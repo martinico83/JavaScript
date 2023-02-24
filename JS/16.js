52782f4d5e3798475edbb5f229256dd036cb9364
Martin <martin.nicolas.os83@gmail.com>
2023-02-24 19:01:24 +0000

    Funciones en JavaScript

---
+++ JS/16.js
@@ -0,0 +1,19 @@
+// Declaracion de funcion
+
+function sumar(){
+  console.log(10+10);
+}
+
+sumar();
+
+// Expresion de la funcion
+
+const sumar2 = function(){
+  console.log(3+3);
+}
+
+sumar2();
+
+
+
+
