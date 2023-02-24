b049739c1ee840311c1d9cd93871d14afc5da7e8
Martin <martin.nicolas.os83@gmail.com>
2023-02-24 19:16:16 +0000

    Diferencias entre el tipo de funciones

--- JS/16.js
+++ JS/16.js
@@ -1,19 +1,29 @@
 // Declaracion de funcion
 
+
 function sumar(){
   console.log(10+10);
 }
 
 sumar();
 
+
 // Expresion de la funcion
+
+/*  Esto va a dar error porque lo toma como una variable
+// JavaScript hace dos pasadas en una toma los valores de las funciones y variables y en la otra //ejecuta
+
+sumar2(); 
+
+const sumar2 = function(){
+  console.log(3+3);
+}
+ */
 
 const sumar2 = function(){
   console.log(3+3);
 }
 
 sumar2();
-
-
 
 
