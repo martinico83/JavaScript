ba517d938a2e99e99b0f8d86715fa021aae70ce2
martinico83 <martin.nicolas.os83@gmail.com>
2023-05-05 13:54:22 -0300

    Estructuras de control

---
+++ JS/22.js
@@ -0,0 +1,43 @@
+/*
+const puntaje = 1000;
+
+if(puntaje == 1000){
+    console.log('Si, el puntaje es de 1000');
+}
+*/
+
+ /*
+const puntaje = 1000;
+
+if(puntaje === 1000){  // Que la comparacion sea exactamente igual
+    console.log('Si, el puntaje es de 1000');
+}else{
+    console.log('No es igual');
+}
+*/
+/*
+if(puntaje !== 1000){
+    console.log('Si es distinto a 1000');
+}else{
+    console.log('Si es 1000');
+}
+*/
+
+// const efectivo = 1000;
+// const carrito = 5500;
+
+// if(efectivo > carrito){
+//     console.log('Si puede pagar');
+// }else{
+//     console.log('No puede pagar');
+// }
+
+const rol = 'EDITOR';
+
+if(rol === 'ADMINISTRADOR'){
+    console.log('Acceso al todo el sistema');
+}else if(rol === 'EDITOR'){
+    console.log('Puedes entrar pero no puedes hacer mucho');
+}else{
+    console.log('NO TIENES ACCESO');
+}
