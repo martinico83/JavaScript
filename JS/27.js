4d419b367369864058113fcd6d9d2800ae9b4252
martinico83 <martin.nicolas.os83@gmail.com>
2023-05-09 13:41:21 -0300

    ¿Que son los Prototypes?

--- JS/27.js
+++ JS/27.js
@@ -9,16 +9,37 @@ const producto = {
 
 // Object Constructor
 
+function Cliente(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
+Cliente.prototype.formatearCliente = function(){
+    return `El Cliente ${this.nombre}, ${this.apellido}.`;
+}
+
 function Producto(nombre, precio, disponible) {
     this.nombre = nombre;
     this.precio = precio;
     this.disponible = disponible;
 }
 
+// Crear funciones que solo se utilizan en un objeto en especifico.
+
+Producto.prototype.formatearProducto = function(){
+    return `El Producto ${this.nombre} tiene un precio de $ ${this.precio}`;
+}
+
+
 const produto2 = new Producto('Monitor curvo 49"', 800, true);
 const produto3 = new Producto('Monitor flexible 29"', 1000, false);
 const produto4 = new Producto('Monitor plegable 27"', 3000, true);
 
-console.log(produto2);
-console.log(produto3);
-console.log(produto4);
+
+const cliente = new Cliente('Martin', 'Orona');
+
+console.log(produto3.formatearProducto());
+
+console.log(produto4.formatearProducto());
+
+console.log(cliente.formatearCliente());
