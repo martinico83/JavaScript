3e58a8fa3116e565d908ad35346e98328b50b54d
martinico83 <martin.nicolas.os83@gmail.com>
2023-05-12 08:39:16 -0300

    Como trabajar con dos consultas async await

--- JS/32.js
+++ JS/32.js
@@ -12,10 +12,29 @@ function descargarNuevosClientes(){
     });
 }
 
+function descargarUltimosPedidos(){
+    return new Promise(resolve => {
+        console.log('Descargando pedidos.....Espere.....');
+
+        setTimeout( () =>  {             
+            resolve('Los pedidos fueron descargados....');
+        }, 3000);
+        
+
+    });
+}
+
 async function app(){
     try {
-        const resultado = await descargarNuevosClientes();
-        console.log(resultado);
+        // const clientes = await descargarNuevosClientes();
+        // const pedidos = await descargarUltimosPedidos();
+        // console.log(clientes);
+        // console.log(pedidos);
+
+        const resultado = await Promise.all( [ descargarNuevosClientes(), descargarUltimosPedidos() ] );
+        console.log(resultado[0]);
+        console.log(resultado[1]);
+
     } catch (error) {
         console.log(error);
     }
