3fde88d4a005024c8def760320e93ec03be02066
martinico83 <martin.nicolas.os83@gmail.com>
2023-05-13 13:42:40 -0300

    Seleccionar Elementos con querySelectorAll

--- JS/scrips.js
+++ JS/scrips.js
@@ -1,2 +1,13 @@
-// alert("Hola Mundo"); 
+//  querySelector
 
+const heading = document.querySelector('a'); // Esto retorna a la consola 1 o 0 elementos
+
+console.log(heading);
+
+
+// querySelectorAll
+
+
+
+
+// getElementById
