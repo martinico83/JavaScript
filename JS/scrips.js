f43c0db950ea9d73d680716b9af992cffdc2a84b
martinico83 <martin.nicolas.os83@gmail.com>
2023-05-17 13:49:58 -0300

    Crear HTML con createElement

--- JS/scrips.js
+++ JS/scrips.js
@@ -17,7 +17,7 @@ const enlaces = document.querySelectorAll('.navegacion a');
 enlaces[2].textContent = 'Nuevo texto para enlace';
 //enlaces[2].href = 'http://google.com';
 enlaces[2].classList.add('nueva-clase');
-enlaces[2].classList.remove('navegacion__enlace');
+//enlaces[2].classList.remove('navegacion__enlace');
 
 
 
@@ -25,3 +25,29 @@ enlaces[2].classList.remove('navegacion__enlace');
 
 const heading2 = document.getElementById('heading');
 console.log(heading2);
+
+// Generar un nuevo enlace
+
+const nuevoEnlace = document.createElement('A')
+
+// Agregar el href
+
+nuevoEnlace.href = 'nuevoEnlace.html';    
+
+// Agregar el texto
+
+nuevoEnlace.textContent = 'Tienda virtual';
+
+// Agregar la clase
+
+nuevoEnlace.classList.add('navegacion__enlace');
+
+// Agregarlo al documento
+
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+
+
+console.log(nuevoEnlace);
+
