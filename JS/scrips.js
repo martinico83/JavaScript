cf3efbda285bd5a35fd2e553a6138d4476980f9c
martinico83 <martin.nicolas.os83@gmail.com>
2023-05-18 13:51:28 -0300

    Reaccionar a Clicks en JavaScrip

--- JS/scrips.js
+++ JS/scrips.js
@@ -78,6 +78,20 @@ console.log(5);
     console.log(2);
 }*/
 
-window.onscroll = function(){
-    console.log('scrolling.....');
+window.onscroll = function(evento){
+    console.log(evento);
 }
+
+
+// Seleccionar elementos y asociarles un evento
+
+const btnEnviar = document.querySelector('.boton--primario');
+
+btnEnviar.addEventListener('click', function(evento){
+    console.log(evento);
+    evento.preventDefault();
+
+    // Validar un formulario
+    console.log('Enviando formulario');
+});
+
